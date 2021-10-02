dae697c58b8d76a7e468a032946ccee9cb44802f
BryantCR <bryancvargas20@gmail.com>
2021-10-01 20:09:49 -0600

    All working good

---
+++ dojos_and_ninjas_app/sql_scripts/dojos_and_ninja.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE dojos_and_ninjas;
+
+USE dojos_and_ninjas;
+
+CREATE TABLE dojos(
+	dojos_id INT PRIMARY KEY NOT NULL,
+    dojos_name VARCHAR(30) NOT NULL,
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
+CREATE TABLE ninjas(
+	id INT PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    age INT,
+    dojos_id INT NOT NULL,
+    FOREIGN KEY (dojos_id) 
+		REFERENCES dojos(dojos_id)
+        ON DELETE CASCADE,
+    created_at DATETIME,
+    updated_at DATETIME
+);
+
+INSERT INTO dojos( dojos_id, dojos_name, created_at, updated_at )
+VALUES  ( 4, 'Cobra', SYSDATE(), SYSDATE() ),
+		( 5, 'Palma', SYSDATE(), SYSDATE() ),
+        ( 6, 'spider', SYSDATE(), SYSDATE() );
+
+INSERT INTO ninjas( id, first_name, last_name, age, dojos_id, created_at, updated_at)
+VALUES  ( 1, 'Alex', 'Miller', 19, 4, SYSDATE(), SYSDATE() ),
+		( 2, 'Michael', 'Garro', 18, 4, SYSDATE(), SYSDATE() ),
+        ( 3, 'Julieta', 'Venegas', 20, 4, SYSDATE(), SYSDATE() );
+INSERT INTO ninjas( id, first_name, last_name, age, dojos_id, created_at, updated_at)
+VALUES  ( 11, 'Juan', 'Miller', 33, 5, SYSDATE(), SYSDATE() ),
+		( 22, 'Tortu', 'Garro', 18, 5, SYSDATE(), SYSDATE() ),
+        ( 33, 'Jason', 'Venegas', 29, 5, SYSDATE(), SYSDATE() );
+INSERT INTO ninjas( id, first_name, last_name, age, dojos_id, created_at, updated_at)
+VALUES  ( 111, 'Juana', 'Miller', 40, 6, SYSDATE(), SYSDATE() ),
+		( 222, 'Kate', 'Garro', 17, 6, SYSDATE(), SYSDATE() ),
+        ( 333, 'Allison', 'Venegas', 25, 6, SYSDATE(), SYSDATE() );
+
